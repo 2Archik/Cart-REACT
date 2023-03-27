6f8b06c563ec22302039396577c620c10bb31c9b
2Archik <artur.avitolog@yandex.ru>
2023-03-28 00:57:38 +0500

    Fixed

--- src/components/CartFooter/index.jsx
+++ src/components/CartFooter/index.jsx
@@ -5,7 +5,7 @@ const CartFooter = ({ total }) => {
   const { count, price } = total;
   return (
     <footer className="cart-footer">
-      <div className="cart-footer__count">{count} ед.</div>
+      <div className="cart-footer__count">{count} шт.</div>
       <div className="cart-footer__price">{formatPrice(price)} руб.</div>
     </footer>
   );
